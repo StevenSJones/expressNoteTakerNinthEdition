b3b992e0ff8e1b8732b7104826901bf588f77ed2
SS Jones <SSJones.code@gmail.com>
2020-09-02 23:36:55 -0700

    I added comments and formatting. As of now the server does work as I have opened localhost:8080 in the browser and see the note taker (get started)  page.

--- server.js
+++ server.js
@@ -12,30 +12,29 @@ const app = express();
 const PORT = 8080;
 //e was npm init, npm express and npm morgan in terminal
 
-//e middleware
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger("dev"));
 
-
 //f add API routes. My data is in a different location (in public file)relative to the other files. SO BE CAREFUL WHEN routing the data
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/public/index.html");
-
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/public/index.html");
 });
+
 //g add API routes
 app.get("/api/notes", function (req, res) {
-    //h readFile (read from the "server")
+  //h readFile (read from the "server")
   res.readFile(__dirname + "/db/db.json", "utf8", function (err, data) {
-    //parse and pass data to the front end
-    const notes = JSON.parse(data); 
+    //i parse and pass data to the front end
+    const notes = JSON.parse(data);
     res.json(notes);
   });
 });
 
-//make out server listen and pass in PORT and a callback function as arguments
-app.listen(PORT, function() {
-console.log("app is listening on PORT: " + PORT);
+//j make out server listen and pass in PORT and a callback function as arguments
+app.listen(PORT, function () {
+  console.log("app is listening on PORT: " + PORT);
 });
 
 //get data
