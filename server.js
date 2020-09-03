aed0f1572361cc75a1d5ddf3f34b5cb0a8f24793
SS Jones <SSJones.code@gmail.com>
2020-09-02 18:51:58 -0700

    I added comments for the sudo code in the server.js file, imported the node module require, ran npm init to install my package.json and to see the dependencies list which include express,installed the npm modules: express and morgan, I called express and set it equal to app and I created a PORT at 8080.

---
+++ server.js
@@ -0,0 +1,20 @@
+//1 - build express server
+//a - node modules that are built in (no terminal install)
+const fs = require("fs");
+
+//b - npm modules that are downloaded (terminal install)
+const express = require("express");
+const logger = require("morgan");
+
+//calling express
+const app = express();
+const PORT = 8080;
+
+//add routes
+
+//my data is in a different location relative to the other files. SO BE CAREFUL WHEN routing the data
+
+//get data
+
+//send data
+
