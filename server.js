545fb3d01db2bc69f95bf98c8c15b17ec6b0736d
SS Jones <SSJones.code@gmail.com>
2020-09-02 19:39:27 -0700

    I fixed some syntax errors and pushed the variables to the top and I am unable to connect with localhost:8080 in the browser still. Going back over notes.

--- server.js
+++ server.js
@@ -6,20 +6,31 @@ const fs = require("fs");
 const express = require("express");
 const logger = require("morgan");
 
-//e middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(logger("dev"));
-
 //c calling express
 const app = express();
 //d creating a port
 const PORT = 8080;
 
-//add routes. My data is in a different location relative to the other files. SO BE CAREFUL WHEN routing the data
+//e middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger("dev"));
 
+
+//add routes. My data is in a different location (in public file)relative to the other files. SO BE CAREFUL WHEN routing the data
+app.get("/", function(req, res) {
+    fs.sendFile(__dirname + "/public/index.html");
+
+});
+
+app.get("/api/notes", function (req, res) {
+  fs.readFile(__dirname + "/db/db.json", "utf8", function (err, data) {
+      //read from the "server"
+    const notes = JSON.parse(data); //parse and pass data to the front end
+    res.json(notes);
+  });
+});
 
 //get data
 
 //send data
-
