0b0d3739f9bce550277d882df942a05ee261daba
SS Jones <SSJones.code@gmail.com>
2020-09-03 21:41:11 -0700

    I included the uuid section in the post and delete routes inside the function.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require("express");
 const logger = require("morgan");
 const { v4: uuidv4 } = require("uuid"); //k to generate a unique id we import uuid
 
-//l to generate a unique id we invoke the id funciton uuidv4() below
+//b to generate a unique id we invoke the id funciton uuidv4() below
 const note = {
   id: uuidv4(),
   title: "Hello",
@@ -20,27 +20,27 @@ const app = express();
 const PORT = 8080;
 //e was npm init, npm express and npm morgan in terminal
 
-// middleware
+// f middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger("dev"));
 app.use(express.static("public")); //builds a route for every file in the public directory.
 
-//f add HTML route. My data is in a different location (in public file)relative to the other files. SO BE CAREFUL WHEN routing the data
+//g add HTML route. My data is in a different location (in public file)relative to the other files. SO BE CAREFUL WHEN routing the data
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-//HTML route for the notes.html page
+//h HTML route for the notes.html page
 app.get("/notes", function (req, res) {
   res.sendFile(__dirname + "/public/notes.html");
 });
 
-//g add API routes
+//i add API routes
 app.get("/api/notes", function (req, res) {
-  //h readFile (read from the "server")
+  //j readFile (read from the "server")
   fs.readFile(__dirname + "/db/db.json", "utf8", function (err, data) {
-    //i parse and pass data to the front end
+    //k parse and pass data to the front end
     const notes = JSON.parse(data);
     res.json(notes);
   });
@@ -52,12 +52,16 @@ app.post("/api/notes", function (req, res) {
   fs.readFile(__dirname + "/db/db.json", "utf8", function (err, data) {
     //n parse data
     const notes = JSON.parse(data);
-    //How do I get my note frm the req obj????
+    //How do I get my note frm the client (browser)?
     // console.log(req);
-    let note = req.body;
+    //b add unique id to the data that we get back
+      const note = {
+        id: uuidv4(),
+        ...req.body,
+      };
     //push parsed data to parsed data array
     notes.push(note);
-    console.log(notes);
+    // console.log(notes);
     //stringify data
     const stringifiedData = JSON.stringify(notes, null, 2);
     //write to server
@@ -66,27 +70,41 @@ app.post("/api/notes", function (req, res) {
       res.json(note);
     });
   });
-  const note = {
-    id: uuidv4(), //add unique id to the data that we get back
-    ...req.body,
-  };
 });
-
+//o the delete route
 app.delete("/api/notes/:id", function (req, res) {
-  console.log("DELETE");
+  const { id } = req.params; //destructure the id off of the params object
+  //n readFile (read from the "server")
+  fs.readFile(__dirname + "/db/db.json", "utf8", function (err, data) {
+    //n parse data
+    let notes = JSON.parse(data);
+    //How do I get my note frm the req obj????
+    // console.log(req);
+    //b add unique id to the data that we get back
+    const note = {
+      id: uuidv4(),
+      ...req.body,
+    };
+    //filter parsed data to parsed data array
+    notes = notes.filter(note => note.id !== id);
+    // console.log(notes);
+    //stringify data
+    const stringifiedData = JSON.stringify(notes, null, 2);
+    //write to server
+    fs.writeFile(__dirname + "/db/db.json", stringifiedData, function () {
+      //n pass data to the front end
+      res.json(true);
+    });
+  });
 });
 
-//__dir name is in place of the file path and adds that to string to allow us to reach the index.html page itself.
+//p __dir name is in place of the file path and adds that to string to allow us to reach the index.html page itself.
 app.all("*", function (req, res) {
   res.sendFile(__dirname + "/public/index.html");
   // console.log("THis far");
 });
 
-//j make out server listen and pass in PORT and a callback function as arguments
+//l make out server listen and pass in PORT and a callback function as arguments
 app.listen(PORT, function () {
   console.log("app is listening on PORT: " + PORT);
 });
-
-//get data
-
-//send data
